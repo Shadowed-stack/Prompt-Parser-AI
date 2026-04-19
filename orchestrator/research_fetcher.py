c8b787878b64b2e65afb390d740fbff77c0a1479
Shadowed-stack <55203700+Shadowed-stack@users.noreply.github.com>
2026-04-19 16:44:36 +0530

    Create research_fetcher.py

---
+++ orchestrator/research_fetcher.py
@@ -0,0 +1,5 @@
+def fetch_research(query):
+    return [
+        {"title": "Paper 1", "finding": "Heat tolerance improves yield"},
+        {"title": "Paper 2", "finding": "Deep roots improve drought resistance"}
+    ]
