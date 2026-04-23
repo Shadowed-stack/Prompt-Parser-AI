d56f74446025336834957ee353694e8ba588d72d
Shadowed-stack <55203700+Shadowed-stack@users.noreply.github.com>
2026-04-24 00:04:19 +0530

    Update similarity_search.py

--- search_engine/similarity_search.py
+++ search_engine/similarity_search.py
@@ -1,7 +1,13 @@
-from .vector_store import collection
-from .embeddings import embed
+from search_engine.vector_store import collection
+from search_engine.embeddings import embed
+from shared.config import settings
 
-def search_traits(query, top_k=3):
-    q = embed(query)
-    res = collection.query(query_embeddings=[q], n_results=top_k)
+def search_traits(query: str):
+    q = embed(query).tolist()
+
+    res = collection.query(
+        query_embeddings=[q],
+        n_results=settings.search_top_k
+    )
+
     return res["documents"][0]
