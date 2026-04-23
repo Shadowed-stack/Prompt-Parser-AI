4834f9e66f047c529762ce7834d8e2ce6db1404e
Shadowed-stack <55203700+Shadowed-stack@users.noreply.github.com>
2026-04-24 00:04:05 +0530

    Update vector_store.py

--- search_engine/vector_store.py
+++ search_engine/vector_store.py
@@ -1,13 +1,22 @@
 import chromadb
 from shared.config import settings
+from search_engine.embeddings import embed
 
 client = chromadb.Client()
 collection = client.get_or_create_collection(settings.chroma_collection)
 
-def add_traits(traits):
+def populate():
+    traits = [
+        "heat tolerance",
+        "drought resistance",
+        "deep root system",
+        "high yield efficiency",
+        "waxy leaf coating"
+    ]
+
     for i, t in enumerate(traits):
         collection.add(
             ids=[str(i)],
             documents=[t],
-            embeddings=[embed(t)]
+            embeddings=[embed(t).tolist()]
         )
