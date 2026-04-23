e9bca7e5690c2678551a88dacc8993a7da764cef
Shadowed-stack <55203700+Shadowed-stack@users.noreply.github.com>
2026-04-24 00:03:50 +0530

    Update embeddings.py

--- search_engine/embeddings.py
+++ search_engine/embeddings.py
@@ -2,5 +2,5 @@ from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def embed(text):
-    return model.encode(text).tolist()
+def embed(text: str):
+    return model.encode(text, normalize_embeddings=True)
