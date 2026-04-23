5bbf8da4e4212517341380929bac3fb119888801
Shadowed-stack <55203700+Shadowed-stack@users.noreply.github.com>
2026-04-24 00:06:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from orchestrator.workflow import run_pipeline
+from search_engine.vector_store import populate
 
 if __name__ == "__main__":
+    populate()
+
     prompt = input("Enter prompt: ")
     result = run_pipeline(prompt)
 
     print("\nFINAL OUTPUT:\n")
-    print(result["spec"])
+    print(result)
